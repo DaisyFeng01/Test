dead5b6d120b3d221c075a238a1b901357150032
unknown <v-danfen@MININT-39TQ26H.fareast.corp.microsoft.com>
2016-01-27 15:57:36 +0800

    [branch-1] add finished log

--- DataBlobStorage1/DataBlobStorage1/Program.cs
+++ DataBlobStorage1/DataBlobStorage1/Program.cs
@@ -75,6 +75,7 @@ namespace DataBlobStorage1Sample
             Console.WriteLine("\nPage Blob Sample");
             BasicStoragePageBlobOperationsAsync().Wait();
 
+            Console.WriteLine("Finished.");
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
         }
